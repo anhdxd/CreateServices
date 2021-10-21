140856c75a7a5104d39c01c0f881f74db9f0e8d6
Vanh <Vanh@DESKTOP-554DJM9>
2021-10-21 19:31:42 +0700

    Add project files.

---
+++ Services/Services.cpp
@@ -0,0 +1,44 @@
+﻿#undef UNICODE 
+
+#include <Windows.h>
+#include <iostream>
+
+#define SVCNAME "SvcAnhdz"
+
+void InstallSvc();
+int main()
+{
+    InstallSvc();
+    
+}
+
+
+void InstallSvc()
+{
+    SC_HANDLE schSCManager, schService; // Handle cua trinh quan ly services va services
+    schSCManager = OpenSCManager(0,0, SC_MANAGER_ALL_ACCESS); // mở trình quản lý
+    if (NULL == schSCManager)
+    {
+        printf("OpenSCManager failed (%d)\n", GetLastError());
+        return;
+    }
+
+    schService = CreateService(schSCManager,
+        SVCNAME, SVCNAME,
+        SERVICE_ALL_ACCESS, SERVICE_WIN32_OWN_PROCESS,
+        SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL, // SERVICE_DEMAND_START: khi gọi hàm startservice thì chạy svc
+        0, 0, 0, 0, 0, 0);
+    if (schService == NULL)
+    {
+        printf("CreateService failed (%d)\n", GetLastError());
+        CloseServiceHandle(schSCManager);
+        return ;
+    }
+    else printf("Service installed successfully\n");
+
+
+
+
+
+    return;
+}
